b8dbc6545e5e12a57d51c29e2e1c56b179ffdf57
arnaud <songac.arnaud@gmail.com>
2017-03-27 11:23:01 -0400

    add files

---
+++ main/chat.py
@@ -0,0 +1,5 @@
+'''
+Created on Mar 27, 2017
+
+@author: arno
+'''
---
+++ main/user.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 27, 2017
+
+@author: arno
+'''
+
+class User:
+    
+    def __init__(self, name):
+        self.name = name
+        return
+    
+    def buildMessage(self, message):
+        
+        return
+    
+    
