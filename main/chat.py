db7b75865abba4db849c3d7b9d9805f0f9329822
arnaud <songac.arnaud@gmail.com>
2017-04-08 14:08:51 -0400

    customize port nb

--- main/chat.py
+++ main/chat.py
@@ -7,13 +7,14 @@ import datetime as dt
 import helper
 import threading
 import os
+import argparse
 '''
 Created on Mar 27, 2017
 
 @author: arno
 '''
 
-PORT = 1025
+DEFAULT_PORT = 1025
 LOCALHOST = "127.0.0.1"
 LOCAL_BROADCAST = "255.255.255.255"
 
@@ -50,48 +51,48 @@ def udpSocket():
     return s
 
 
-def sender(user):
+def sender(user, port):
     user.name = raw_input("Enter your name: ")
     print("Welcome " + user.name + " to the chat room. You can now chat!")
     s = udpSocket()
 
     joinMessage = user.buildMessage(Command.JOIN, ''.join([user.name, ' joined!']))
-    s.sendto(joinMessage.encode(), (LOCAL_BROADCAST, PORT))
+    s.sendto(joinMessage.encode(), (LOCAL_BROADCAST, port))
 
     while True:
         raw = raw_input()
         command = parseUserCommand(raw)
         
         if command is not None:
-            handleUserCommand(s, user, command[0], command[1])
+            handleUserCommand(s, port, user, command[0], command[1])
 
         else:
             message = user.buildMessage(Command.TALK, raw)
-            s.sendto(message.encode(), (LOCAL_BROADCAST, PORT))
+            s.sendto(message.encode(), (LOCAL_BROADCAST, port))
 
 
-def receiver(user):
+def receiver(user, port):
 
     s = udpSocket()
     try:
-        s.bind(('', PORT))
+        s.bind(('', port))
 
         while True:
             msgBytes, address = s.recvfrom(4096)
             m = helper.parse_message(str(msgBytes.decode()))
-            handleMessageReceived(s,user, address, m[0], m[1], m[2])
+            handleMessageReceived(s, port, user, address, m[0], m[1], m[2])
     finally:
         s.close()
 
 
-def handleMessageReceived(soc, curUser, senderAddress, senderName, command, message):
+def handleMessageReceived(soc, port, curUser, senderAddress, senderName, command, message):
     cur_date_formatted = re.sub('T', ' ', dt.datetime.now().isoformat())
 
     if Command.JOIN == command:
         print(''.join([cur_date_formatted, ' ', str(senderName), ' joined!']))
         ChatRoom.addUser(senderName, senderAddress[0])
         pingMessage = curUser.buildMessage(Command.PING, "")
-        soc.sendto(pingMessage.encode(), (LOCAL_BROADCAST, PORT))
+        soc.sendto(pingMessage.encode(), (LOCAL_BROADCAST, port))
 
     elif Command.TALK == command:
         print(''.join([cur_date_formatted, ' [', str(senderName), ']: ', message]))
@@ -124,16 +125,16 @@ def parseUserCommand(message):
     return None
 
 
-def handleUserCommand(soc, user, action, content):
+def handleUserCommand(soc, port, user, action, content):
     if Command.LEAVE == action:
         leaveMessage = user.buildMessage(Command.LEAVE, '')
         quitMessage = user.buildMessage(Command.QUIT, '')
-        soc.sendto(leaveMessage.encode(), (LOCAL_BROADCAST, PORT))
-        soc.sendto(quitMessage.encode(), (LOCALHOST, PORT))
+        soc.sendto(leaveMessage.encode(), (LOCAL_BROADCAST, port))
+        soc.sendto(quitMessage.encode(), (LOCALHOST, port))
         
     elif Command.WHO == action:
         message = user.buildMessage(Command.WHO, '')
-        soc.sendto(message.encode(), (LOCALHOST, PORT))
+        soc.sendto(message.encode(), (LOCALHOST, port))
         
     else:
         print("Command not found")
@@ -141,10 +142,22 @@ def handleUserCommand(soc, user, action, content):
         
 
 if __name__ == '__main__':
-    print("Starting chat")
+    
+    args = argparse.ArgumentParser(description="Chat room on the LAN")
+    helpMessage = 'Port number on which chat runs, default is ' + str(DEFAULT_PORT)
+    args.add_argument('-p', '--port', action='store', dest='port', type=int, metavar="", default=DEFAULT_PORT, help=helpMessage)
+    parser = args.parse_args()
+    
+    port = int(parser.port)
+    
+    if port <= 1024:
+        print ("Forbidden port")
+        os._exit(1)
+    
+    print("Starting chat on port " + str(port))
     user = User("")
-    threading.Thread(target=receiver, args=[user]).start()
-    threading.Thread(target=sender, args=[user]).start()
+    threading.Thread(target=receiver, args=[user, port]).start()
+    threading.Thread(target=sender, args=[user, port]).start()
 
 
 
