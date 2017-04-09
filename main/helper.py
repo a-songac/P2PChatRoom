bb8652c6c9125115411622506e4fec454a09627d
arnaud <songac.arnaud@gmail.com>
2017-04-09 19:31:49 -0400

    helper for command line interface inputs

--- main/helper.py
+++ main/helper.py
@@ -13,3 +13,26 @@ def parse_message(message):
 
     return content
 
+
+def choiceFromRange(choices):
+
+    print("Please select one among the following:")
+    for i, val in enumerate(choices):
+        print(''.join([str(i+1), ". ", str(val)]))
+    
+    choice = raw_input("Your choice: ")
+    wrong = not isInt(choice) or int(choice) < 1 or int(choice) > len(choices)
+        
+    while wrong:
+        choice = raw_input("Wrong selection, please retry: ")
+        wrong = not isInt(choice) or int(choice) < 1 or int(choice) > len(choices)
+    return int(choice)
+            
+            
+def isInt(value):
+    try:
+        int(value)
+        return True
+    except:
+        return False
+    
